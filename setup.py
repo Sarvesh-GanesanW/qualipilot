f348030915bcb46a4e0b23379f6bd8fc5a106a28
Sarvesh-GanesanW <sarveshganesanwork@gmail.com>
2024-06-21 20:31:01 +0530

    .

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='datawhiz',  
-    version='0.1.1',  
+    name='DataSanitizer',  
+    version='1',  
     packages=find_packages(),
     install_requires=[
         'pandas',
@@ -19,5 +19,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.11',
+    python_requires='>=3.10',
 )
